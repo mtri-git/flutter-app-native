19875d8dce6066218d18f13a867b79995db16652
mtri-git <vmtri20@gmail.com>
2023-03-13 17:32:15 +0700

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import AVAudioPlayer
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,26 +8,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    AudioHandler.register(with: self)
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-class AudioHandler: NSObject, FlutterPlugin {
-    private static var channel: FlutterMethodChannel?
-    private var player: AVAudioPlayer?
-
-    static func register(with registrar: FlutterPluginRegistrar) {
-        channel = FlutterMethodChannel(name: "audio_player", binaryMessenger: registrar.messenger())
-        let instance = AudioHandler()
-        registrar.addMethodCallDelegate(instance, channel: channel!)
-    }
-
-    func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch call.method {
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let audioChannel = FlutterMethodChannel(name: "audio_player",
+                                              binaryMessenger: controller.binaryMessenger)
+    audioChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      // This method is invoked on the UI thread.
+      // Handle battery messages.
+      switch call.method {
         case "play":
-            if let urlString = call.arguments as? String, let url = URL(string: urlString) {
+            if let urlString = call.arguments("url") as? String, let url = URL(string: urlString) {
                 let session = AVAudioSession.sharedInstance()
                 do {
                     try session.setCategory(.playback, mode: .default, options: [])
@@ -50,5 +42,50 @@ class AudioHandler: NSObject, FlutterPlugin {
         default:
             result(FlutterMethodNotImplemented)
         }
-    }
+    })
+
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
+
+// class AudioHandler: NSObject, FlutterPlugin {
+//     private static var channel: FlutterMethodChannel?
+//     private var player: AVAudioPlayer?
+
+//     static func register(with registrar: FlutterPluginRegistrar) {
+//         channel = FlutterMethodChannel(name: "audio_player", binaryMessenger: registrar.messenger())
+//         let instance = AudioHandler()
+//         registrar.addMethodCallDelegate(instance, channel: channel!)
+//     }
+
+//     func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+//         switch call.method {
+//         case "play":
+//             if let urlString = call.arguments as? String, let url = URL(string: urlString) {
+//                 let session = AVAudioSession.sharedInstance()
+//                 do {
+//                     try session.setCategory(.playback, mode: .default, options: [])
+//                     try session.setActive(true)
+//                     player = try AVAudioPlayer(contentsOf: url)
+//                     player?.play()
+//                     result("success")
+//                 } catch {
+//                     result("error")
+//                 }
+//             } else {
+//                 result("error")
+//             }
+//         case "pause":
+//             player?.pause()
+//             result("success")
+//         case "stop":
+//             player?.stop()
+//             player = nil
+//             result("success")
+//         default:
+//             result(FlutterMethodNotImplemented)
+//         }
+//     }
+// }
