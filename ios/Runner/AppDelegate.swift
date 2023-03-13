f7cbd959d75a5cb4804d6ed17c415d994a6d8d72
mtri-git <vmtri20@gmail.com>
2023-03-13 15:13:05 +0700

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 import AudioPlayer
+import AVFoundation
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -44,3 +46,33 @@ import AudioPlayer
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+class AudioPlayer: NSObject, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    func play(url: URL) {
+        stop()
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.delegate = self
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        } catch {
+            print("Error playing audio: \(error)")
+        }
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+    }
+    
+    func stop() {
+        audioPlayer?.stop()
+        audioPlayer = nil
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        stop()
+    }
+}
