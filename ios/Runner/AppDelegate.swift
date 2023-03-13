ae1a21604f7ef73a6c725e4b7ed20e9ae25673a2
mtri-git <vmtri20@gmail.com>
2023-03-13 15:31:08 +0700

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,39 +10,40 @@ import AVFoundation
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller: FlutterViewController = windows?.rootViewController as! FlutterViewController
-    let audioChanel = FlutterMethodChannel(name: "audio_player", binaryMessenger: controller.binaryMessenger)
-    audioChanel.setMethodCallHandler({weak self}
-    (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      
-      audioChanel.setMethodCallHandler({
-      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let audioChanel = FlutterMethodChannel(name: "audio_player",
+                                              binaryMessenger: controller.binaryMessenger)
+        batteryChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
       // This method is invoked on the UI thread.
-      // guard call.method == "play" else {
-      //   result(FlutterMethodNotImplemented)
-      //   return
-      // }
-      switch call.method {
-      case "play":
-          let url = call.arguments("url")
-          AudioPlayer.play(url)
-          result(null)
-      case "pause":
-        AudioPlayer.pause()
+        audioChanel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        // This method is invoked on the UI thread.
+        // guard call.method == "play" else {
+        //   result(FlutterMethodNotImplemented)
+        //   return
+        // }
+        switch call.method {
+        case "play":
+            let url = call.arguments("url")
+            AudioPlayer.play(url)
+            result(null)
+        case "pause":
+          AudioPlayer.pause()
+            result(null)
+
+        case "stop":
+          AudioPlayer.stop()
           result(null)
 
-      case "stop":
-        AudioPlayer.stop()
-        result(null)
+        default:
+          result(FlutterMethodNotImplemented)
+          
+        }
+      })
 
-      default:
-        result(FlutterMethodNotImplemented)
-        
-      }
     })
-
-
-    )
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
