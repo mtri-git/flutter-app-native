7003c7e857c3b1b2e2a3bbf48c094fe2baab13df
mtri-git <vmtri20@gmail.com>
2023-03-13 16:23:22 +0700

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,31 +2,29 @@ import UIKit
 import Flutter
 import AVFoundation
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  private let audioChannelName = "audio_player"
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    AudioHandler.register(with: self.flutterEngine!, channelName: audioChannelName)
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+class AppDelegate: FlutterAppDelegate {
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        AudioHandler.register(with: self)
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
 
 
-@objc class AudioHandler: NSObject, FlutterPlugin {
+class AudioHandler: NSObject, FlutterPlugin {
     private static var channel: FlutterMethodChannel?
     private var player: AVAudioPlayer?
 
-    static func register(with registrar: FlutterPluginRegistrar, channelName: String) {
-        channel = FlutterMethodChannel(name: channelName, binaryMessenger: registrar.messenger())
+    static func register(with registrar: FlutterPluginRegistrar) {
+        channel = FlutterMethodChannel(name: "audio_player", binaryMessenger: registrar.messenger())
         let instance = AudioHandler()
         registrar.addMethodCallDelegate(instance, channel: channel!)
     }
 
-    func handle(_ call: FlutterMethodCall, result: @escaping @escaping FlutterResult) {
+    func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "play":
             if let urlString = call.arguments as? String, let url = URL(string: urlString) {
